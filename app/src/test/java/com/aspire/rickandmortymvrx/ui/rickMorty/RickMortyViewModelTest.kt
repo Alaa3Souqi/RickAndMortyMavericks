c01c8f0e8c40f5950acccaa614bbf9daebe284d7
alaa <alaasouqi4@gmail.com>
2022-05-12 14:28:09 +0300

    add unit test

---
+++ app/src/test/java/com/aspire/rickandmortymvrx/ui/rickMorty/RickMortyViewModelTest.kt
@@ -0,0 +1,34 @@
+package com.aspire.rickandmortymvrx.ui.rickMorty
+
+import com.airbnb.mvrx.Success
+import com.airbnb.mvrx.test.MvRxTestRule
+import com.airbnb.mvrx.withState
+import com.aspire.rickandmortymvrx.mavericks.CharacterState
+import com.aspire.rickandmortymvrx.modles.Character
+import com.aspire.rickandmortymvrx.modles.CharacterResponse
+import org.junit.Assert
+import org.junit.Rule
+import org.junit.Test
+
+class RickMortyViewModelTest {
+
+    @get:Rule
+    val mvrxRule = MvRxTestRule()
+
+    @Test
+    fun `fetch character when create view model`() {
+
+        val charactersList = listOf(Character("alaa", ""))
+
+        val repo = mockk<RickMortyRepository> {
+            coEvery { getCharacters("1") } returns CharacterResponse(charactersList)
+        }
+
+        val viewModel = RickMortyViewModel(CharacterState(), repo)
+
+        withState(viewModel) { state ->
+            Assert.assertEquals(Success(CharacterResponse(charactersList)), state.state)
+        }
+
+    }
+}
